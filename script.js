a194f72261b5ba4a926004c8791c9d053f4ae0d2
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-04-22 19:43:13 +0200

    switch light and dark mode ( change text, change icon and colors )

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
 
-const themeIcon = document.getElementById('themeIcon');
+const toggleIcon = document.getElementById('toggle-icon');
 
-// Event Listener
-toggleSwitch.addEventListener('change', (event) => {
+
+// Dark Mode Style
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50% )';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+}
+
+// Light Mode
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50% )';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+}
+
+// Switch Theme Dynamically
+
+function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        themeIcon.classList.remove('fa-sun');
-        themeIcon.classList.add('fa-moon');
+        darkMode();
+        // toggleIcon.classList.remove('fa-sun');
+        // toggleIcon.classList.add('fa-moon');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        themeIcon.classList.remove('fa-moon');
-        themeIcon.classList.add('fa-sun');
+        lightMode();
+        // toggleIcon.classList.remove('fa-moon');
+        // toggleIcon.classList.add('fa-sun');
     }
-});
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
